e7ddc99d2cff1650d348f938c95e2e367413f76d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-16 13:58:56 +0300

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,42 +11,57 @@ import {
 const Navbar = () => {
   return (
     <AppBar position="static" color="default" elevation={1}>
-      <Container maxWidth="lg">
-        <Toolbar
+      {/* <Container maxWidth="xl"> */}
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          px: 2,
+          maxWidth: "xl",
+        }}
+      >
+        <Box
           sx={{
             display: "flex",
-            justifyContent: "space-between",
+            flexDirection: { xs: "column", md: "row" },
             alignItems: "center",
+            gap: 3,
           }}
         >
-          <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
-            <Typography variant="h6" color="textPrimary" fontWeight="bold">
-              codementor
-            </Typography>
+          <Typography variant="h6" color="textPrimary" fontWeight="bold">
+            codementor
+          </Typography>
 
-            <Button color="inherit">Mentorship ▼</Button>
-            <Button color="inherit">Expert help ▼</Button>
-            <Button color="inherit">Freelancing ▼</Button>
-            <Button color="inherit">More ▼</Button>
-          </Box>
+          <Button color="inherit">Mentorship ▼</Button>
+          <Button color="inherit">Expert help ▼</Button>
+          <Button color="inherit">Freelancing ▼</Button>
+          <Button color="inherit">More ▼</Button>
+        </Box>
 
-          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-            <Button color="inherit">Become a mentor</Button>
-            <Button color="inherit">Log in</Button>
-            <Button
-              variant="contained"
-              sx={{
-                bgcolor: "black",
-                "&:hover": { bgcolor: "grey.800" },
-                px: 3,
-                py: 1,
-              }}
-            >
-              Sign Up
-            </Button>
-          </Box>
-        </Toolbar>
-      </Container>
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            alignItems: "center",
+            gap: 2,
+          }}
+        >
+          <Button color="inherit">Become a mentor</Button>
+          <Button color="inherit">Log in</Button>
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "black",
+              "&:hover": { bgcolor: "grey.800" },
+              px: 3,
+              py: 1,
+            }}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Toolbar>
+      {/* </Container> */}
     </AppBar>
   );
 };
